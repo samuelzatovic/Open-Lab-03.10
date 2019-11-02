e657f5cb584ebbd4d98f56c10d75e2e5fe6cbe3c
samuel zaťovič <szatovic@gmail.com>
2019-11-02 18:49:58 +0100

    Hotový program

--- Open-Lab-03.10/Checker.cs
+++ Open-Lab-03.10/Checker.cs
@@ -6,6 +6,7 @@ namespace Open_Lab_03._10
     {
         public int GetNumberOfCharsInString(char letter, string str)
         {
+            int result = str.Count(a, a == letter);
             throw new NotImplementedException();
         }
     }
